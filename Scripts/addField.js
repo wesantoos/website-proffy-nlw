bbdd8253f0af4a5442f8d66a8f6df024f30877e1
Wesley S <wesley.qq@live.com>
2020-08-06 14:02:43 -0300

    Update addField.js

--- Scripts/addField.js
+++ Scripts/addField.js
@@ -1,11 +1,11 @@
 const button = document.querySelector("#add-time");
 button.addEventListener('click', cloneField);
 
-function cloneField(){
+function cloneField() {
     const newFieldContainer = document.querySelector(".schedule-item").cloneNode(true);
     const fields = newFieldContainer.querySelectorAll('input');
 
-    fields.forEach(function(field){
+    fields.forEach(function (field) {
         field.value = ""
     })
 

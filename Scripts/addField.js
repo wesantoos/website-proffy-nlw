3a92a56d51f25d7d94acd21d6a7ce1827a56c324
Wesley S <wesley.qq@live.com>
2020-08-06 14:02:04 -0300

    start js

---
+++ Scripts/addField.js
@@ -0,0 +1,13 @@
+const button = document.querySelector("#add-time");
+button.addEventListener('click', cloneField);
+
+function cloneField(){
+    const newFieldContainer = document.querySelector(".schedule-item").cloneNode(true);
+    const fields = newFieldContainer.querySelectorAll('input');
+
+    fields.forEach(function(field){
+        field.value = ""
+    })
+
+    document.querySelector("#schedule-items").appendChild(newFieldContainer);
+}
